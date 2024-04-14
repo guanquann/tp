e4d14fccdd1f14b4bd1773b8cf12f079a4064d25
Guan Quan <76832850+guanquann@users.noreply.github.com>
2024-04-14 22:15:55 +0800

    Revert back default sample data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -18,23 +18,25 @@ import seedu.address.model.tag.Tag;
  * Contains utility methods for populating {@code AddressBook} with sample data.
  */
 public class SampleDataUtil {
+    // Reverted back to original code in v1.3
+    // Refer to https://github.com/nus-cs2103-AY2324S2/forum/issues/843
     public static Person[] getSamplePersons() {
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #01-40"), new Company("Alex Catering"),
+                new Address("Blk 30 Geylang Street 29, #06-40"), new Company("Alex Catering"),
                 getTagSet("catering")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens Market, #01-18"), new Company("Big Porks"),
+                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Company("Big Porks"),
                 getTagSet("fresh", "pork")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #01-04"), new Company("Oliveiro Olives"),
+                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Company("Oliveiro Olives"),
                 getTagSet("olives")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #01-43"), new Company("Chicken and Egg"),
+                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Company("Chicken and Egg"),
                 getTagSet("eggs", "chicken")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #01-35"), new Company("Fresh Frozen"),
-                getTagSet("frozen", "fresh"))
+                new Address("Blk 47 Tampines Street 20, #17-35"), new Company("Fresh Frozen"),
+                getTagSet("frozen", "unfresh"))
         };
     }
 
